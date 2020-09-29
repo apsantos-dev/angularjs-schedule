0df13682496a2a8307b0eadb31a0194b2141cce0
APSantos Dev <apsantos.dev@gmail.com>
2020-09-29 18:28:45 -0300

    Added social network icons

--- web/src/controllers/HomeCtrl.js
+++ web/src/controllers/HomeCtrl.js
@@ -1,3 +1,7 @@
 app.controller("HomeCtrl", function ($scope) {
   $scope.message = "Lista de contatos com integração a API do GitHub";
+
+  $scope.openURL = (link) => {
+    console.log("link", link);
+  };
 });
